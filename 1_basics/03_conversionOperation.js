c53a83d700aca28333c13a694a5e4250ae1f6822
RITIK RAMOLA <69358426+ritikramola@users.noreply.github.com>
2024-08-16 07:58:07 +0000

    Conversion

---
+++ 1_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = undefined
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);  //let score = "33abc"  output//NaN
+// console.log(valueInNumber);  //let score = null  output//0
+// console.log(valueInNumber);  //let score = undefined  output//NaN
+
+// "33" => 33
+// "33abc" => NaN
+//  true=> 1; false =>0
+
+let isLoggedIn = "ritik"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+// 1=> true
+// 0=> false
+// "" => false
+// "ritik" => true
+
+let someNumber = 22
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
