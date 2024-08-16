929c146619f7b653986eced463c02bab168923aa
RITIK RAMOLA <69358426+ritikramola@users.noreply.github.com>
2024-08-16 07:32:10 +0000

    datatypes

---
+++ 1_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";  // treat all js as newer version
+
+//alert(3+3)  //will be used in browser
+
+let name = "ritik"
+let age = 20
+let isLoggedIn = false
+
+
+
+//number =>2 power 53
+//bigint
+//string = ""
+//boolean =>true/false
+//null
+// undefined
+// symbol =>uniqueness
+
+
+//object 
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
