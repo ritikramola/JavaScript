8a536e68b86790bb64a4cc31e019a84963596460
RITIK RAMOLA <69358426+ritikramola@users.noreply.github.com>
2024-08-16 06:59:16 +0000

    var

---
+++ 1_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId= 101
+let accountEmail = "ritik@google.com"
+var accountPassword = "12345"
+
+accountCity = "Doon"
+
+//const can't be changed throughout the program
+
+//accountId = 200
+
+console.log(accountId);
