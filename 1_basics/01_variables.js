0cfb380f0ecdd4ea289b2a1e114d282183008469
RITIK RAMOLA <69358426+ritikramola@users.noreply.github.com>
2024-08-16 07:10:50 +0000

    complete variables

--- 1_basics/01_variables.js
+++ 1_basics/01_variables.js
@@ -1,11 +1,21 @@
 const accountId= 101
 let accountEmail = "ritik@google.com"
 var accountPassword = "12345"
-
-accountCity = "Doon"
+accountCity = "Doon"  //can be done,but don't use
+let accountState;   //it will show undefined as output
 
 //const can't be changed throughout the program
-
 //accountId = 200
 
+accountEmail = "hc@gmail.com"
+accountPassword = "11111"
+accountCity = "delhi"
+
 console.log(accountId);
+
+/**
+ * prefer not to use var
+ * because of issue in block and function scope
+ */
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
