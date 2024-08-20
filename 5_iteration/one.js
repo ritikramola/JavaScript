118405e44a0dc5e606c2e8fb16d36f7758707725
RITIK RAMOLA <69358426+ritikramola@users.noreply.github.com>
2024-08-20 09:37:09 +0000

    loops

---
+++ 5_iteration/one.js
@@ -0,0 +1,14 @@
+//for loop
+
+
+
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    console.log(element)
+}
+
+//console.log(element);
+
+//break and continue used to stop code in between
+
+
