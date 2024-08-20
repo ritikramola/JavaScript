aca5562283094ce616a361154e7ad3479cc14d41
RITIK RAMOLA <69358426+ritikramola@users.noreply.github.com>
2024-08-20 08:11:02 +0000

    control flow

---
+++ 4_controlFlow/switch.js
@@ -0,0 +1,47 @@
+//switch (key)
+// {
+//     case value:
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("february");
+            break;
+    case 3:
+        console.log("march");
+           break;        
+
+    default:
+        console.log("default");
+        
+        break;
+}
+//for string
+
+const month1 = "jan"
+
+switch (month1) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("february");
+            break;
+    case "mar":
+        console.log("march");
+           break;        
+
+    default:
+        console.log("default");
+        
+        break;
+}
---
+++ 4_controlFlow/truth.js
@@ -0,0 +1,50 @@
+// const userEmail = "ritik@g.com"
+
+// if(userEmail){
+//     console.log("Got user email");//this will run
+// }else{
+//     console.log("don't have user email");
+// }  
+
+
+// const userEmail = ""
+
+// if(userEmail){
+//     console.log("Got user email");
+// }else{
+//     console.log("don't have user email"); //this will run
+// }
+
+
+const userEmail = []
+
+if(userEmail){
+    console.log("Got user email"); //this will run
+}else{
+    console.log("don't have user email");
+}
+
+//falsy values:
+
+//false , 0 , -0 ,BigInt (0n),null , undefined , NaN , ""
+
+//truthy values
+
+//"0",'false'," ",[],{},function(){},
+
+//empty arrays
+// if(userEmail.length === 0){
+//     console.log("Array is empty"); //it will run
+    
+// }
+
+//empty objects
+
+// const emptyObj = {}
+
+// if(Object.keys(emptyObj).length === 0){
+//     console.log("Object is empty");
+    
+// }
+
+// false == 0,false =='',0==''
